2dd8c26756f1ee846ba7d74a736d37734f7d9d85
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-01 18:12:22 +0300

    Optimized filters

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -3,29 +3,25 @@ import { TCountry } from "../App";
 
 interface IFilterProps {
   countries: TCountry[];
-  countriesSmallerThanLtu: TCountry[];
   setCountriesSmallerThanLtu: React.Dispatch<React.SetStateAction<TCountry[]>>;
+  setCountriesInOceania: React.Dispatch<React.SetStateAction<TCountry[]>>;
 }
 
-const Filter = ({ countries, countriesSmallerThanLtu, setCountriesSmallerThanLtu }: IFilterProps) => {
-  
+const Filter = ({ countries, setCountriesSmallerThanLtu, setCountriesInOceania }: IFilterProps) => {
   const findCountriesSmallerThanLtu = () => {
     const areaOfLithuania = 65300;
+    setCountriesSmallerThanLtu(countries.filter(country => country.area < areaOfLithuania));
+  };
 
-    for (let i = 0; i < countries.length; i++) {
-      if (countries[i].area < areaOfLithuania) {
-        countriesSmallerThanLtu.push(countries[i]);
-      }
-    }
-    setCountriesSmallerThanLtu([...countriesSmallerThanLtu]);
-    // console.log("Countries smaller than Ltu:", countriesSmallerThanLtu);
+  const findCountriesInOceania = () => {
+    setCountriesInOceania(countries.filter(country => (country.region = "Oceania")));
   };
 
   return (
     <div className="filter-options">
       <label>Filter options:</label>
       <button onClick={findCountriesSmallerThanLtu}>Countries are smaller than Lithuania</button>
-      <button>Countries are in Oceania</button>
+      <button onClick={findCountriesInOceania}>Countries are in Oceania</button>
     </div>
   );
 };
