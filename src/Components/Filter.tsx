534947edbe47486289e6bd4ceceb3b296fb6cc72
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-07-17 18:41:02 +0300

    Updated props for filter and sort after pagination

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { TCountry } from '../App';
+import React from "react";
+import { TCountry } from "../App";
 
 interface IFilterProps {
-  countries: TCountry[];
+  currentCountries: TCountry[];
   areaRef: React.MutableRefObject<HTMLButtonElement | null>;
   regionRef: React.MutableRefObject<HTMLButtonElement | null>;
   activeButton: string;
@@ -10,24 +10,27 @@ interface IFilterProps {
   setShowCountries: React.Dispatch<React.SetStateAction<TCountry[]>>;
 }
 
-const Filter = ({ countries, areaRef, regionRef, activeButton, setActiveButton, setShowCountries }: IFilterProps) => {
-
+const Filter = ({ currentCountries, areaRef, regionRef, activeButton, setActiveButton, setShowCountries }: IFilterProps) => {
   const findCountriesSmallerThanLtu = () => {
     const areaOfLithuania = 65300;
-    setShowCountries(countries.filter(country => country.area < areaOfLithuania));
-    setActiveButton(areaRef?.current?.id  || "");
+    setShowCountries(currentCountries.filter(country => country.area < areaOfLithuania));
+    setActiveButton(areaRef?.current?.id || "");
   };
 
   const findCountriesInOceania = () => {
-    setShowCountries(countries.filter(country => country.region === "Oceania"));
-    setActiveButton(regionRef?.current?.id  || "");
+    setShowCountries(currentCountries.filter(country => country.region === "Oceania"));
+    setActiveButton(regionRef?.current?.id || "");
   };
 
   return (
     <div className="filter-options">
       <label>Filter options:</label>
-      <button ref={areaRef} id="filter-area"onClick={findCountriesSmallerThanLtu}>Countries are smaller than Lithuania</button>
-      <button ref={regionRef} id="filter-region" onClick={findCountriesInOceania}>Countries are in Oceania</button>
+      <button ref={areaRef} id="filter-area" onClick={findCountriesSmallerThanLtu}>
+        Countries are smaller than Lithuania
+      </button>
+      <button ref={regionRef} id="filter-region" onClick={findCountriesInOceania}>
+        Countries are in Oceania
+      </button>
     </div>
   );
 };
--- src/Components/Sort.tsx
+++ src/Components/Sort.tsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import { TCountry } from "../App";
 
 interface ISortProps {
-  countries: TCountry[];
+  currentCountries: TCountry[];
   sortRef: React.MutableRefObject<HTMLButtonElement | null>;
   activeButton: string;
   setActiveButton: React.Dispatch<React.SetStateAction<string>>;
   setShowCountries: React.Dispatch<React.SetStateAction<TCountry[]>>;
 }
 
-const Sort = ({ countries, sortRef, activeButton, setActiveButton, setShowCountries }: ISortProps) => {
+const Sort = ({ currentCountries, sortRef, activeButton, setActiveButton, setShowCountries }: ISortProps) => {
   const [sortDesc, setSortDesc] = useState(true);
 
   const sortHandler = () => {
-    countries.sort((a, b) => {
+    currentCountries.sort((a, b) => {
       if (sortDesc) {
         if (a["name"] < b["name"]) {
           return 1;
@@ -26,7 +26,7 @@ const Sort = ({ countries, sortRef, activeButton, setActiveButton, setShowCountr
       return -1;
     });
     setActiveButton(sortRef?.current?.id || "");
-    setShowCountries([...countries]);
+    setShowCountries([...currentCountries]);
     setSortDesc(!sortDesc);
   };
 
